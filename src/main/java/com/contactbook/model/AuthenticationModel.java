1e34214f4909df987930df15bd7e039652178115
Midhun Balan <midhun.balan@anywhere.co>
2021-05-29 16:30:28 +0530

    added lombok for the pojo class

--- src/main/java/com/contactbook/model/AuthenticationModel.java
+++ src/main/java/com/contactbook/model/AuthenticationModel.java
@@ -1,9 +1,13 @@
 package com.contactbook.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
+@Getter
+@Setter
 @Component
 public class AuthenticationModel {
     private String firstName;
@@ -13,60 +17,4 @@ public class AuthenticationModel {
     private String encryptedPassword;
     private String contactNumber;
     private Map<String, String> address;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmailId() {
-        return emailId;
-    }
-
-    public void setEmailId(String emailId) {
-        this.emailId = emailId;
-    }
-
-    public String getProductId() {
-        return productId;
-    }
-
-    public void setProductId(String productId) {
-        this.productId = productId;
-    }
-
-    public String getEncryptedPassword() {
-        return encryptedPassword;
-    }
-
-    public void setEncryptedPassword(String encryptedPassword) {
-        this.encryptedPassword = encryptedPassword;
-    }
-
-    public String getContactNumber() {
-        return contactNumber;
-    }
-
-    public void setContactNumber(String contactNumber) {
-        this.contactNumber = contactNumber;
-    }
-
-    public Map<String, String> getAddress() {
-        return address;
-    }
-
-    public void setAddress(Map<String, String> address) {
-        this.address = address;
-    }
 }
